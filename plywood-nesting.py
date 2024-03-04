c977f7a2afadd2ff5e49f237900d7c0c374672da
fkcurrie <72585240+fkcurrie@users.noreply.github.com>
2024-03-03 22:02:22 -0500

    Update plywood-nesting.py

--- plywood-nesting.py
+++ plywood-nesting.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+<Your Python File Name>.py: Description of what the script does.
+
+Author: Frank Currie
+Date: March 3rd, 2024
+Version: 0.1
+License: Apache License 2.0
+"""
+
+# Apache License 2.0
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import svgwrite
 from typing import List, Dict
 import csv
